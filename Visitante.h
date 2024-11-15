14eae7edf1f40aa9ed8534cb29d0cfb362645cd9
Fabio Lopes <fabiolopes7844@gmail.com>
2024-11-15 10:08:21 -0300

    Add Visitante class for visitor management with entry and exit methods

---
+++ Visitante.h
@@ -0,0 +1,31 @@
+#ifndef VISITANTE_H
+#define VISITANTE_H
+
+#include "Usuario.h"
+#include "Data.h"
+
+class Visitante : Usuario
+{
+
+private:
+    int id;
+    string nome;
+    Data* inicio;
+    Data* fim;
+
+public:
+    Visitante(int id, string nome, Data* inicio, Data* fim);
+    virtual ~Visitante();
+    
+    Data* getDataInicio();
+    Data* getDataFim();
+
+    bool entrar(Data *d);
+    bool sair(Data *d);
+    bool registrarEntradaManual(Data* d);
+    bool registrarSaidaManual(Data* d);
+
+
+};
+
+#endif
