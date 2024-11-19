d795a684149f286583747da6dd9af34793704f92
Fabio Lopes <fabiolopes7844@gmail.com>
2024-11-19 19:01:54 -0300

    Enhance error handling in menu functionality and improve user input processing for loading and saving users

--- menu.cpp
+++ menu.cpp
@@ -35,19 +35,25 @@ void menu()
     cout << "Deseja carregar usuarios (s/n): ";
     cin >> resposta;
 
+    if (resposta == 's')
+    {
 
-    if(resposta == 's'){
         cout << "Arquivo: ";
         cin >> arquivo;
-        
         PersistenciaDeUsuario *p = new PersistenciaDeUsuario();
+        try
+        {
+            vector<Usuario *> *usuarios = p->carregar(arquivo);
 
-        vector<Usuario*> *usuarios = p->carregar(arquivo);
-
-        for(int i = 0; i < usuarios->size(); i++){
-            g->adicionar((*usuarios)[i]);
+            for (int i = 0; i < usuarios->size(); i++)
+            {
+                g->adicionar((*usuarios)[i]);
+            }
         }
-
+        catch(logic_error *e)
+        {
+            cout << e->what() << endl << endl;
+        }
     }
 
     do
@@ -96,48 +102,58 @@ void menu()
                 cout << "[Entrada] Catraca " << catraca << " travada" << endl
                      << endl;
             }
-            else{
-                cout << "[Entrada] Catraca " << catraca << " abriu: id " << id << endl
-                     << endl;
-            }
-        break;
-        }
-
-        case 2:
-        {
-            cout << "Catraca: ";
-            cin >> catraca;
-            cout << "Id: ";
-            cin >> id;
-            cout << "Hora: ";
-            cin >> hora;
-            cout << "Minuto: ";
-            cin >> minuto;
-            cout << "Segundo: ";
-            cin >> segundo;
-            cout << "dia: ";
-            cin >> dia;
-            cout << "mes: ";
-            cin >> mes;
-            cout << "Ano: ";
-            cin >> ano;
-
-            d = new Data(hora, minuto, segundo, dia, mes, ano);
-
-            if (c1->sair(id, d) != true)
+            else
             {
-                cout << "[Saida] Catraca " << catraca << " travada" << endl
-                     << endl;
-            }
-            else{
-                cout << "[Saida] Catraca " << catraca << " abriu: id " << id << endl
+                cout << "[Entrada] Catraca " << catraca << " abriu: id " << id << endl
                      << endl;
             }
             break;
         }
 
+        case 2:
+        {
+            try{
+                cout << "Catraca: ";
+                cin >> catraca;
+                cout << "Id: ";
+                cin >> id;
+                cout << "Hora: ";
+                cin >> hora;
+                cout << "Minuto: ";
+                cin >> minuto;
+                cout << "Segundo: ";
+                cin >> segundo;
+                cout << "dia: ";
+                cin >> dia;
+                cout << "mes: ";
+                cin >> mes;
+                cout << "Ano: ";
+                cin >> ano;
+
+                d = new Data(hora, minuto, segundo, dia, mes, ano);
+
+                if (c1->sair(id, d) != true)
+                {
+                    cout << "[Saida] Catraca " << catraca << " travada" << endl
+                         << endl;
+                }
+                else
+                {
+                    cout << "[Saida] Catraca " << catraca << " abriu: id " << id << endl
+                         << endl;
+                }
+            }
+            catch(invalid_argument *e)
+            {
+                cout << e->what() << endl << endl;
+            }
+            
+            break;
+        }
+
         case 3:
         {
+            try {
             cout << "Entrada (e) ou Saida (s)? ";
             cin >> opcao;
 
@@ -160,85 +176,99 @@ void menu()
 
             // condicao
             cout << "Entrada manual registrada: id " << id << endl;
+            }
+            catch(invalid_argument *e)
+            {
+                cout << e->what() << endl << endl;
+            }
 
             break;
         }
 
         case 4:
         {
-
-            cout << "Tipo (v, a ou f): ";
-            cin >> tipo;
-
-            if (tipo == 'v')
+            try
             {
-                Data *dataInicio;
-                Data *dataFim;
+                cout << "Tipo (v, a ou f): ";
+                cin >> tipo;
 
-                cout << "Id: ";
-                cin >> id;
-                cout << "Nome: ";
-                cin >> nome;
-                cout << "Data de inicio" << endl;
-                cout << "Hora: ";
-                cin >> hora;
-                cout << "Minuto: ";
-                cin >> minuto;
-                cout << "Segundo: ";
-                cin >> segundo;
-                cout << "dia: ";
-                cin >> dia;
-                cout << "mes: ";
-                cin >> mes;
-                cout << "Ano: ";
-                cin >> ano;
+                if (tipo == 'v')
+                {
+                    Data *dataInicio;
+                    Data *dataFim;
 
-                dataInicio = new Data(hora, minuto, segundo, dia, mes, ano);
+                    cout << "Id: ";
+                    cin >> id;
+                    cout << "Nome: ";
+                    cin >> nome;
+                    cout << "Data de inicio" << endl;
+                    cout << "Hora: ";
+                    cin >> hora;
+                    cout << "Minuto: ";
+                    cin >> minuto;
+                    cout << "Segundo: ";
+                    cin >> segundo;
+                    cout << "dia: ";
+                    cin >> dia;
+                    cout << "mes: ";
+                    cin >> mes;
+                    cout << "Ano: ";
+                    cin >> ano;
 
-                cout << "Data de fim" << endl;
-                cout << "Hora: ";
-                cin >> hora;
-                cout << "Minuto: ";
-                cin >> minuto;
-                cout << "Segundo: ";
-                cin >> segundo;
-                cout << "dia: ";
-                cin >> dia;
-                cout << "mes: ";
-                cin >> mes;
-                cout << "Ano: ";
-                cin >> ano;
+                    dataInicio = new Data(hora, minuto, segundo, dia, mes, ano);
 
-                dataFim = new Data(hora, minuto, segundo, dia, mes, ano);
+                    cout << "Data de fim" << endl;
+                    cout << "Hora: ";
+                    cin >> hora;
+                    cout << "Minuto: ";
+                    cin >> minuto;
+                    cout << "Segundo: ";
+                    cin >> segundo;
+                    cout << "dia: ";
+                    cin >> dia;
+                    cout << "mes: ";
+                    cin >> mes;
+                    cout << "Ano: ";
+                    cin >> ano;
 
-                Visitante *v = new Visitante(id, nome, dataInicio, dataFim);
-                g->adicionar(v);
-                cout << "Visitante cadastrado com sucesso";
+                    dataFim = new Data(hora, minuto, segundo, dia, mes, ano);
+
+                    Visitante *v = new Visitante(id, nome, dataInicio, dataFim);
+                    g->adicionar(v);
+                    cout << "Visitante cadastrado com sucesso" << endl
+                         << endl;
+                }
+                if (tipo == 'a')
+                {
+                    cout << "Id: ";
+                    cin >> id;
+                    cout << "Nome: ";
+                    cin >> nome;
+
+                    aluno = new Aluno(id, nome);
+                    g->adicionar(aluno);
+                    cout << "Aluno cadastrado com sucesso" << endl
+                         << endl;
+                }
+
+                if (tipo == 'f')
+                {
+                    cout << "Id: ";
+                    cin >> id;
+                    cout << "Nome: ";
+                    cin >> nome;
+
+                    funcionario = new Funcionario(id, nome);
+                    g->adicionar(funcionario);
+                    cout << "Funcionario cadastrado com sucesso" << endl
+                         << endl;
+                }
             }
-            if (tipo == 'a')
+            catch (invalid_argument *e)
             {
-                cout << "Id: ";
-                cin >> id;
-                cout << "Nome: ";
-                cin >> nome;
-
-                aluno = new Aluno(id, nome);
-                g->adicionar(aluno);
-                cout << "Aluno cadastrado com sucesso";
+                cout << e->what() << endl
+                     << endl;
             }
-
-            if (tipo == 'f')
-            {
-                cout << "Id: ";
-                cin >> id;
-                cout << "Nome: ";
-                cin >> nome;
-
-                funcionario = new Funcionario(id, nome);
-                g->adicionar(funcionario);
-                cout << "Funcionario cadastrado com sucesso" << endl << endl;
-            }
-
             break;
         }
 
@@ -249,7 +279,8 @@ void menu()
             cout << "Ano: ";
             cin >> ano;
 
-            cout << endl <<"Relatorio de horas trabalhadas" << endl;
+            cout << endl
+                 << "Relatorio de horas trabalhadas" << endl;
 
             for (int i = 0; i < g->getUsuarios()->size(); i++)
             {
@@ -257,7 +288,7 @@ void menu()
                 if (func != nullptr)
                 {
                     cout << func->getNome() << ": " << func->getHorasTrabalhadas(mes, ano) << endl;
-                }        
+                }
             }
             cout << endl;
             break;
@@ -280,6 +311,19 @@ void menu()
         {
             cout << "Deseja salvar usuarios (s/n)";
             cin >> resposta;
+
+            if (resposta == 's')
+            {
+                cout << "Arquivo: ";
+                cin >> arquivo;
+                PersistenciaDeUsuario *p = new PersistenciaDeUsuario();
+                try{
+                    p->salvar(arquivo, g->getUsuarios());
+
+                }catch(logic_error *e){
+                    cout << e->what() << endl << endl;
+                }
+            }
 
             // codigo salvando usuarios
 
